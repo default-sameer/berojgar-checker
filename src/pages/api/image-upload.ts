e7bfc10f371c66207195bfb671eb06664e1ac25b
default-sameer <sameerjoshicr7@gmail.com>
2022-06-13 14:38:58 +0545

    Supabase Image Upload Paused

--- src/pages/api/image-upload.ts
+++ src/pages/api/image-upload.ts
@@ -9,50 +9,51 @@ export default async  function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    const supabaseUrl = process.env.SUPABASE_URL as string;
-    if(req.method === 'POST') {
-      let {image} = req.body;
-      if(!image){
-        res.status(400).json({error: 'Image is required'});
-        return;
-      }
-      try {
-        const contentType = image.match(/data:(.*);base64/)?.[1];
-        const base64FileData = image.split('base64,')?.[1];
+    // const supabaseUrl = process.env.SUPABASE_URL as string;
+    // if(req.method === 'POST') {
+    //   let {image} = req.body;
+    //   if(!image){
+    //     res.status(400).json({error: 'Image is required'});
+    //     return;
+    //   }
+    //   try {
+    //     const contentType = image.match(/data:(.*);base64/)?.[1];
+    //     const base64FileData = image.split('base64,')?.[1];
 
-        if(!contentType || !base64FileData) {
-          res.status(400).json({message: 'Invalid image'});
-          return;
-        }
+    //     if(!contentType || !base64FileData) {
+    //       res.status(400).json({message: 'Invalid image'});
+    //       return;
+    //     }
 
-        // UploadImage
-        const fileName = nanoid();
-        const ext = contentType.split('/')[1];
-        const path = `${fileName}.${ext}`;
+    //     // UploadImage
+    //     const fileName = nanoid();
+    //     const ext = contentType.split('/')[1];
+    //     const path = `${fileName}.${ext}`;
 
-        const {data , error: uploadError} = await supabase.storage.from(process.env.SUPABASE_BUCKET as string).upload(path, decode(base64FileData),{
-            contentType,
-            upsert: true
-        });
-        if (uploadError) {
-          console.log(uploadError);
-          throw new Error('Unable to upload image to storage');
-        }
+    //     const {data , error: uploadError} = await supabase.storage.from(process.env.SUPABASE_BUCKET as string).upload(path, decode(base64FileData),{
+    //         contentType,
+    //         upsert: true
+    //     });
+    //     if (uploadError) {
+    //       console.log(uploadError);
+    //       throw new Error('Unable to upload image to storage');
+    //     }
 
-        // Constructing public url
-        const url = `${supabaseUrl.replace(
-        '.co',
-        '.in'
-        // @ts-ignore
-        )}/storage/v1/object/public/${data.Key}`;
+    //     // Constructing public url
+    //     const url = `${supabaseUrl.replace(
+    //     '.co',
+    //     '.in'
+    //     // @ts-ignore
+    //     )}/storage/v1/object/public/${data.Key}`;
 
-        return res.status(200).json({ url });
-      } catch (error) {
-        res.status(500).json({message: 'Something went wrong'});
-      }
-    }
-    else{
-      res.setHeader('Allow', ['POST']);
-      res.status(405).json({ message: `HTTP method ${req.method} is not supported.` });
-    }
+    //     return res.status(200).json({ url });
+    //   } catch (error) {
+    //     res.status(500).json({message: 'Something went wrong'});
+    //   }
+    // }
+    // else{
+    //   res.setHeader('Allow', ['POST']);
+    //   res.status(405).json({ message: `HTTP method ${req.method} is not supported.` });
+    // }
+    res.status(200).json({ message: 'Comming Soon' })
 }
